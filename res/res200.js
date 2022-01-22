266782fbe9b8c83f3f8c0a4fb6b0ba91d0d3a2c9
rezihardi <rezydoerasta@gmail.com>
2022-01-22 18:07:05 +0700

    tes

--- res/res200.js
+++ res/res200.js
@@ -9,3 +9,13 @@ exports.ok = function(values, res) {
   res.json(data);
   res.end();
 };
+
+exports.gaha = function(values, res) {
+  let data = {
+      'status': "true",
+      'message': "data ditampilkan",
+      'data': values
+  };
+  res.json(data);
+  res.end();
+};
